1ec69533fb65cbe43a5d9477b561017cff705cf6
Santaravind <aravindsant1310@gmail.com>
2026-08-20 16:12:01 +0530

    Start Learning dynamin programming

---
+++ Arrays/Split_Array_Largest_Sum_410.java
@@ -0,0 +1,4 @@
+package DSA.Arrays;
+
+public class Split_Array_Largest_Sum_410 {
+}
---
+++ Dynamic_Programming/Fibonacci.java
@@ -0,0 +1,23 @@
+package DSA.Dynamic_Programming;
+
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static int fib(int n, int dp[]){
+        if (n<=1) return n;
+
+        if (dp[n]!=-1) return dp[n];
+
+        dp[n]=fib(n-1,dp)+fib(n-2,dp);
+        return dp[n];
+
+    }
+    public static void main(String[] args) {
+        int n=4;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+
+        System.out.println(fib(n,dp));
+
+    }
+}
